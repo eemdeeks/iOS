25a93bd8f321a17a3fe0f19a846e5e4beb5fb92d
eemdeeks <prince9088@gmail.com>
2022-11-22 18:59:53 +0900

    인터페이스 빌더로 화면 구성하기

---
+++ boostcourse/iOS 앱 프로그래밍/1. 음원 재생기 애플리케이션/MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 박승찬 on 2022/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
