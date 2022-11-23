a1f115e9e9ab68e6708cc7399f6b510c91762987
eemdeeks <prince9088@gmail.com>
2022-11-23 23:51:14 +0900

    인터페이스 빌더의 객체를 코드와 연결(IBOutlet)

--- boostcourse/iOS 앱 프로그래밍/1. 음원 재생기 애플리케이션/MusicPlayer/MusicPlayer/ViewController.swift
+++ boostcourse/iOS 앱 프로그래밍/1. 음원 재생기 애플리케이션/MusicPlayer/MusicPlayer/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK : IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 }
 
