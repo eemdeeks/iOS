8c56cc3e5a52ff95ceb4d9bb8c1bf26cf729c1a6
eemdeeks <prince9088@gmail.com>
2023-03-05 21:23:19 +0900

    프리온 보딩 사전과제(UI 완성)

---
+++ 원티드/프리온보딩 2차 사전과제/LoadImage/LoadImage/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  LoadImage
+//
+//  Created by 박승찬 on 2023/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var imgView1: UIImageView!
+    @IBOutlet weak var imgView2: UIImageView!
+    @IBOutlet weak var imgView3: UIImageView!
+    @IBOutlet weak var imgView4: UIImageView!
+    @IBOutlet weak var imgView5: UIImageView!
+    
+    
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
