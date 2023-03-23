813bae2e8039c7c249bf9ffcb1c8d924357b644d
eemdeeks <prince9088@gmail.com>
2023-03-23 21:31:00 +0900

    Building List and Navigation

---
+++ swiftUI/SwiftUI Tutorials/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 박승찬 on 2023/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI/SwiftUI Tutorials/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 박승찬 on 2023/03/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }.navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max", "iPad Pro (12.9-inch)(6nd generation)"], id: \.self) { deviceName in
+                    LandmarkList()
+                        .previewDevice(PreviewDevice(rawValue: deviceName))
+                        .previewDisplayName(deviceName)
+                }
+    }
+}
