7c262a4c8cc45f42f50db18f0baaf6c71c2229f7
eemdeeks <prince9088@gmail.com>
2023-03-26 22:44:04 +0900

    MC1 test

---
+++ swiftUI/toy/testMC1/testMC1/SecondView.swift
@@ -0,0 +1,33 @@
+//
+//  SecondView.swift
+//  testMC1
+//
+//  Created by 박승찬 on 2023/03/25.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        VStack{
+            
+            Spacer()
+            NavigationView{
+                VStack{
+                    NavigationLink(destination:QuestionList()){
+                        Text("Skip")
+                    }
+                    TalkView()
+                }
+            }
+            
+        }
+        
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
---
+++ swiftUI/toy/testMC1/testMC1/testMC1App.swift
@@ -0,0 +1,17 @@
+//
+//  testMC1App.swift
+//  testMC1
+//
+//  Created by 박승찬 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct testMC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
