0643f517f63e975736a9ede159193c8897c1f203
eemdeeks <prince9088@gmail.com>
2023-04-20 02:57:28 +0900

    AppIcon 설정

--- swiftUI/toy/LSKW.swiftpm/Package.swift
+++ swiftUI/toy/LSKW.swiftpm/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             teamIdentifier: "37UUYN2CGZ",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .weights),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.purple),
             supportedDeviceFamilies: [
                 .pad,
