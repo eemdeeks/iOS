c020599df0bee8c7f5475a7c912f5631058a5b5c
eemdeeks <prince9088@gmail.com>
2023-06-16 14:52:14 +0900

    toy: (calculator) 다크모드고정

---
+++ swiftUI/toy/calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by 박승찬 on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
