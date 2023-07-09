a114373291a392209b6c30d6a3dd3d112617b030
eemdeeks <prince9088@gmail.com>
2023-07-09 15:00:37 +0900

    CloudKit project 초기설정

---
+++ Resources/CloudKit/CloudKitProject/CloudKitProject/CloudKitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitProjectApp.swift
+//  CloudKitProject
+//
+//  Created by 박승찬 on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Resources/CloudKit/CloudKitProject/CloudKitProject/CloudKitUser.swift
@@ -0,0 +1,20 @@
+//
+//  CloudKitUser.swift
+//  CloudKitProject
+//
+//  Created by 박승찬 on 2023/07/09.
+//
+
+import SwiftUI
+
+struct CloudKitUser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CloudKitUser_Previews: PreviewProvider {
+    static var previews: some View {
+        CloudKitUser()
+    }
+}
