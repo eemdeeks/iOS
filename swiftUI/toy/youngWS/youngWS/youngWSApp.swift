1b1a7eca5ebbde7016e8287838bca52e502e8d7b
eemdeeks <prince9088@gmail.com>
2023-09-27 19:55:15 +0900

    영워크샵

---
+++ swiftUI/toy/youngWS/youngWS/youngWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  youngWSApp.swift
+//  youngWS
+//
+//  Created by 박승찬 on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct youngWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
