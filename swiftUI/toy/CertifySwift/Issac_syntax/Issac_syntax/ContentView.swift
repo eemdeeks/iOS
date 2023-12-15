8758aa5bf5da744fd8f6ce6ade88dbd33027423f
eemdeeks <prince9088@gmail.com>
2023-12-15 17:18:10 +0900

    아이작세션저장용

---
+++ swiftUI/toy/CertifySwift/Issac_syntax/Issac_syntax/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Issac_syntax
+//
+//  Created by 박승찬 on 11/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI/toy/CertifySwift/Issac_syntax/Issac_syntax/Issac_syntaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Issac_syntaxApp.swift
+//  Issac_syntax
+//
+//  Created by 박승찬 on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Issac_syntaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            subject()
+        }
+    }
+}
