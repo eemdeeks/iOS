683c6a0bdfae183bb1abd2616db4453abe5159a7
eemdeeks <prince9088@gmail.com>
2024-03-22 19:22:21 +0900

    @escaping

--- swiftUI/Data flow/Swift Concurrency/SwiftConcurrency/SwiftConcurrency/ContentView.swift
+++ swiftUI/Data flow/Swift Concurrency/SwiftConcurrency/SwiftConcurrency/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            DownloadImageAsync()
         }
         .padding()
     }
---
+++ swiftUI/Data flow/Swift Concurrency/SwiftConcurrency/SwiftConcurrency/DownloadImageAsync.swift
@@ -0,0 +1,65 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConcurrency
+//
+//  Created by 박승찬 on 3/22/24.
+//
+
+import SwiftUI
+
+class DownloadImageAsyncImageLoader {
+    let url = URL(string: "https://picsum.photos/200")!
+
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
+        guard let data = data,
+              let image = UIImage(data: data),
+              let response = response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            return nil
+        }
+        return image
+    }
+
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image, nil)
+        }
+        .resume()
+    }
+}
+
+class DownloadImageAsyncViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    let loader = DownloadImageAsyncImageLoader()
+
+    func fetchImage() {
+        loader.downloadWithEscaping { [weak self] image, error in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+    }
+}
+
+struct DownloadImageAsync: View {
+    @StateObject private var viewModel = DownloadImageAsyncViewModel()
+
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            }
+        }
+        .onAppear {
+            viewModel.fetchImage()
+        }
+    }
+}
+
+#Preview {
+    DownloadImageAsync()
+}
