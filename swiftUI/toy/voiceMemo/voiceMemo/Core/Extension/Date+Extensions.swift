1b8cce3e7c36548daec6b685e19e5d17207acbf5
eemdeeks <prince9088@gmail.com>
2024-06-28 18:37:08 +0900

    Date extensions
    작삼프로젝트 Day-10

--- swiftUI/toy/voiceMemo/voiceMemo/Core/Extension/Date+Extensions.swift
+++ swiftUI/toy/voiceMemo/voiceMemo/Core/Extension/Date+Extensions.swift
@@ -6,5 +6,28 @@
 import Foundation
 
 extension Date {
-  
+    var formattedTime: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "a hh:mm"
+        return formatter.string(from: self)
+    }
+
+    var formattedDay: String {
+        let now = Date()
+        let calender = Calendar.current
+
+        let nowStartOfDay = calender.startOfDay(for: now)
+        let dateStartOfDay = calender.startOfDay(for: self)
+        let numOfDaysDifference = calender.dateComponents([.day], from: nowStartOfDay, to: dateStartOfDay).day!
+
+        if numOfDaysDifference == 0 {
+            return "오늘"
+        } else {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateFormat = "M월 d일 E요일"
+            return formatter.string(from: self)
+        }
+    }
 }
