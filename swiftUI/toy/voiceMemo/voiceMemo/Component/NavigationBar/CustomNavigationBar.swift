100ef92466f1e5b357593202fbf962050605c855
eemdeeks <prince9088@gmail.com>
2024-07-02 23:35:50 +0900

    custom navigation bar
    작삼프로젝트 Day-12

--- swiftUI/toy/voiceMemo/voiceMemo/Component/NavigationBar/CustomNavigationBar.swift
+++ swiftUI/toy/voiceMemo/voiceMemo/Component/NavigationBar/CustomNavigationBar.swift
@@ -6,9 +6,53 @@
 import SwiftUI
 
 struct CustomNavigationBar: View {
-  var body: some View {
-    Text("CustomNavigationBar")
-  }
+    let isDisplayLeftBtn: Bool
+    let isDisplayRightBtn: Bool
+    let leftBtnAction: () -> Void
+    let rightBtnAction: () -> Void
+    let rightBtnType: NavigationBtnType
+
+    init(
+        isDisplayLeftBtn: Bool = true,
+        isDisplayRightBtn: Bool = true,
+        leftBtnAction: @escaping () -> Void = {},
+        rightBtnAction: @escaping () -> Void = {},
+        rightBtnType: NavigationBtnType = .edit
+    ) {
+        self.isDisplayLeftBtn = isDisplayLeftBtn
+        self.isDisplayRightBtn = isDisplayRightBtn
+        self.leftBtnAction = leftBtnAction
+        self.rightBtnAction = rightBtnAction
+        self.rightBtnType = rightBtnType
+    }
+
+    var body: some View {
+        HStack {
+            if isDisplayLeftBtn {
+                Button(
+                    action: leftBtnAction,
+                    label: { Image("leftArrow") }
+                )
+            }
+
+            Spacer()
+
+            if isDisplayRightBtn {
+                Button(
+                    action: rightBtnAction,
+                    label: {
+                        if rightBtnType == .close {
+                            Image("close")
+                        } else {
+                            Text(rightBtnType.rawValue)
+                                .foregroundStyle(Color.customBlack)
+                        }
+                })
+            }
+        }
+        .padding(.horizontal, 20)
+        .frame(height: 20)
+    }
 }
 
 struct CustomNavigationBar_Previews: PreviewProvider {
--- swiftUI/toy/voiceMemo/voiceMemo/Component/NavigationBar/NavigationBtnType.swift
+++ swiftUI/toy/voiceMemo/voiceMemo/Component/NavigationBar/NavigationBtnType.swift
@@ -5,6 +5,9 @@
 
 import Foundation
 
-enum NavigationBtnType {
-  
+enum NavigationBtnType: String {
+    case close
+    case edit = "편집"
+    case complete = "완료"
+    case create = "생성"
 }
